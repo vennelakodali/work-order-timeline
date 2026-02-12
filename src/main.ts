c5ef1d255608a4e47c9b77d2f0a8a88be214b3db
Vennela Kodali <vennelakodali@gmail.com>
2026-02-11 19:10:29 -0600

    Styles fixes; Adds localize support.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import '@angular/localize/init';
 
 bootstrapApplication(AppComponent, {
   providers: [
